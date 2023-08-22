7ad860a32762eef0480948aa73674e83c138ffa4
erinne1 <141894676+erinne1@users.noreply.github.com>
2023-08-22 15:40:06 -0700

    Add files via upload

---
+++ lab04/lab04.py
@@ -0,0 +1,172 @@
+def flatten(s): 
+    """Returns a flattened version of list s.
+
+    >>> flatten([1, 2, 3])     # normal list
+    [1, 2, 3]
+    >>> x = [1, [2, 3], 4]     # deep list
+    >>> flatten(x)
+    [1, 2, 3, 4]
+    >>> x # Ensure x is not mutated
+    [1, [2, 3], 4]
+    >>> x = [[1, [1, 1]], 1, [1, 1]] # deep list
+    >>> flatten(x)
+    [1, 1, 1, 1, 1, 1]
+    >>> x
+    [[1, [1, 1]], 1, [1, 1]]
+    >>> x = [[1, [2, 3], [4, [5, 6]]]]
+    >>> flatten(x)
+    [1, 2, 3, 4, 5, 6]
+    >>> x
+    [[1, [2, 3], [4, [5, 6]]]]
+    >>> x = [[1, [1, [1, [1, 1, [1, 1, [1]]]], 1]]]
+    >>> flatten(x)
+    [1, 1, 1, 1, 1, 1, 1, 1, 1]
+    >>> x
+    [[1, [1, [1, [1, 1, [1, 1, [1]]]], 1]]]
+    """
+    "*** YOUR CODE HERE ***"
+    x = []
+    for item in s:
+        if type(item) == list:
+            x+= flatten(item)
+        else:
+            x+= [item]    
+    return x
+
+
+def my_map(fn, seq):
+    """Applies fn onto each element in seq and returns a list.
+    >>> my_map(lambda x: x*x, [1, 2, 3])
+    [1, 4, 9]
+    >>> my_map(lambda x: abs(x), [1, -1, 5, 3, 0])
+    [1, 1, 5, 3, 0]
+    >>> my_map(lambda x: print(x), ['cs61a', 'summer', '2023'])
+    cs61a
+    summer
+    2023
+    [None, None, None]
+    """
+    return [fn(m) for m in seq ]
+
+
+def my_filter(pred, seq):
+    """Keeps elements in seq only if they satisfy pred.
+    >>> my_filter(lambda x: x % 2 == 0, [1, 2, 3, 4])  # new list has only even-valued elements
+    [2, 4]
+    >>> my_filter(lambda x: (x + 5) % 3 == 0, [1, 2, 3, 4, 5])
+    [1, 4]
+    >>> my_filter(lambda x: print(x), [1, 2, 3, 4, 5])
+    1
+    2
+    3
+    4
+    5
+    []
+    >>> my_filter(lambda x: max(5, x) == 5, [1, 2, 3, 4, 5, 6, 7])
+    [1, 2, 3, 4, 5]
+    """
+
+    return [v for v in seq if pred(v)]
+    
+    #list adds v, iterating through every item in the sequence 
+
+
+def my_reduce(combiner, seq):
+    """Combines elements in seq using combiner.
+    seq will have at least one element.
+    >>> my_reduce(lambda x, y: x + y, [1, 2, 3, 4])  # 1 + 2 + 3 + 4
+    10
+    >>> my_reduce(lambda x, y: x * y, [1, 2, 3, 4])  # 1 * 2 * 3 * 4
+    24
+    >>> my_reduce(lambda x, y: x * y, [4])
+    4
+    >>> my_reduce(lambda x, y: x + 2 * y, [1, 2, 3]) # (1 + 2 * 2) + 2 * 3
+    11
+    """
+    "*** YOUR CODE HERE ***"
+    y = seq[0]
+    for x in seq[1:]:
+        y = combiner(y, x)
+    return y
+    
+    
+
+    
+
+
+def my_map_syntax_check():
+    """Check that your two_of_three code consists of nothing but a return statement.
+
+    >>> # You aren't expected to understand the code of this test.
+    >>> import inspect, ast
+    >>> [type(x).__name__ for x in ast.parse(inspect.getsource(my_map)).body[0].body]
+    ['Expr', 'Return']
+    """
+    # You don't need to edit this function. It's just here to check your work.
+    
+
+
+def my_filter_syntax_check():
+    """Check that your two_of_three code consists of nothing but a return statement.
+
+    >>> # You aren't expected to understand the code of this test.
+    >>> import inspect, ast
+    >>> [type(x).__name__ for x in ast.parse(inspect.getsource(my_filter)).body[0].body]
+    ['Expr', 'Return']
+    """
+    # You don't need to edit this function. It's just here to check your work.
+
+
+def merge(lst1, lst2):
+    """Merges two sorted lists.
+
+    >>> s1 = [1, 3, 5]
+    >>> s2 = [2, 4, 6]
+    >>> merge(s1, s2)
+    [1, 2, 3, 4, 5, 6]
+    >>> s1
+    [1, 3, 5]
+    >>> s2
+    [2, 4, 6]
+    >>> merge([], [2, 4, 6])
+    [2, 4, 6]
+    >>> merge([1, 2, 3], [])
+    [1, 2, 3]
+    >>> merge([5, 7], [2, 4, 6])
+    [2, 4, 5, 6, 7]
+    >>> merge([2, 3, 4], [2, 4, 6])
+    [2, 2, 3, 4, 4, 6]
+    """
+    "*** YOUR CODE HERE ***"
+    bothLst=lst1+lst2
+    mergesorted=[]  #the RV is this new list
+    
+    while bothLst:
+        mergesorted.append(min(bothLst))
+        bothLst.remove(min(bothLst))
+    return mergesorted
+
+
+
+'''CS61A Su2023 Solutions for merge via Recursion'''
+def merge2(lst1,lst2):
+    if not lst1 or not lst2:
+        return lst1+lst2
+    elif lst1[0]<lst2[0]:
+        return [lst1[0]]+merge2(lst1[1:], lst2)
+    return [lst2[0]] + merge2(lst1,lst2[1:])
+    
+
+
+'''CS61A Su2023 Solutions for count_palindromes'''
+
+def count_palindromes(L):
+    """The number of palindromic words in the sequence of strings
+    L (ignoring case).
+
+    >>> count_palindromes(("Acme", "Madam", "Pivot", "Pip"))
+    2
+    """
+    "*** YOUR CODE HERE ***"
+    return len(list(filter(lambda s: s.lower() == s[::-1].lower(), L)))
+    
---
+++ lab06/lab06.py
@@ -0,0 +1,636 @@
+from operator import add, mul
+
+square = lambda x: x * x
+
+identity = lambda x: x
+
+triple = lambda x: 3 * x
+
+increment = lambda x: x + 1
+
+
+HW_SOURCE_FILE = __file__
+
+
+def prune_leaves(t, vals):
+    """Return a modified copy of t with all leaves that have a label
+    that appears in vals removed.  Return None if the entire tree is
+    pruned away.
+
+    >>> t = tree(2)
+    >>> print(prune_leaves(t, (1, 2)))
+    None
+    >>> numbers = tree(1, [tree(2), tree(3, [tree(4), tree(5)]), tree(6, [tree(7)])])
+    >>> print_tree(numbers)
+    1
+      2
+      3
+        4
+        5
+      6
+        7
+    >>> print_tree(prune_leaves(numbers, (3, 4, 6, 7)))
+    1
+      2
+      3
+        5
+      6
+    """
+    "*** YOUR CODE HERE ***"
+    if is_leaf(t) and (label(t) in vals):   #base case
+        return None             
+    new_branches = []                       #creating empty list
+    for b in branches(t):
+        new_branch = prune_leaves(b, vals)  #b is the smaller tree from t
+        if new_branch:                      # if new_branch is True, it will be added
+            new_branches += [new_branch]
+    return tree(label(t), new_branches)
+        
+
+
+def ordered_digits(x):
+    """Return True if the (base 10) digits of X>0 are in non-decreasing
+    order, and False otherwise.
+
+    >>> ordered_digits(5)
+    True
+    >>> ordered_digits(11)
+    True
+    >>> ordered_digits(127)
+    True
+    >>> ordered_digits(1357)
+    True
+    >>> ordered_digits(21)
+    False
+    >>> result = ordered_digits(1375) # Return, don't print
+    >>> result
+    False
+
+    """
+    "*** YOUR CODE HERE ***"
+    def helper(n):
+        if n<10:
+            return True
+        elif n%10 < n%100//10: # if most recent digit is less than the next one
+            return False
+        return helper(n//10)
+
+    return helper(x)
+
+
+def get_k_run_starter(n, k):
+    """Returns the 0th digit of the kth increasing run within n.
+    >>> get_k_run_starter(123444345, 0) # example from description
+    3
+    >>> get_k_run_starter(123444345, 1)
+    4
+    >>> get_k_run_starter(123444345, 2)
+    4
+    >>> get_k_run_starter(123444345, 3)
+    1
+    >>> get_k_run_starter(123412341234, 1)
+    1
+    >>> get_k_run_starter(1234234534564567, 0)
+    4
+    >>> get_k_run_starter(1234234534564567, 1)
+    3
+    >>> get_k_run_starter(1234234534564567, 2)
+    2
+    """
+    i = 0
+    final = None
+    while i<=k:
+        while n>10 and (n%10 > (n//10)%10):
+            n//=10
+        final = n%10
+        i = i+1
+        n = n//10
+    return final
+
+
+def composer(func1, func2):
+    """Returns a function f, such that f(x) = func1(func2(x))."""
+    def f(x):
+        return func1(func2(x))
+    return f
+
+
+def make_repeater(func, n):
+    """Returns the function that computes the nth application of func.
+
+    >>> add_three = make_repeater(increment, 3)
+    >>> add_three(5)
+    8
+    >>> make_repeater(triple, 5)(1) # 3 * 3 * 3 * 3 * 3 * 1
+    243
+    >>> make_repeater(square, 2)(5) # square(square(5))
+    625
+    >>> make_repeater(square, 4)(5) # square(square(square(square(5))))
+    152587890625
+    >>> make_repeater(square, 0)(5) # Yes, it makes sense to apply the function zero times!
+    5
+    """
+    "*** YOUR CODE HERE ***"
+    return accumulate(composer, lambda x: x, n, lambda k: func)
+    
+
+
+def div_by_primes_under(n):
+    """
+    >>> div_by_primes_under(10)(11)
+    False
+    >>> div_by_primes_under(10)(121)
+    False
+    >>> div_by_primes_under(10)(12)
+    True
+    >>> div_by_primes_under(5)(1)
+    False
+    """
+    checker = lambda x: False
+    i = ____________________________
+    while ____________________________:
+        if not checker(i):
+            checker = ____________________________
+        i = ____________________________
+    return ____________________________
+
+
+def div_by_primes_under_no_lambda(n):
+    """
+    >>> div_by_primes_under_no_lambda(10)(11)
+    False
+    >>> div_by_primes_under_no_lambda(10)(121)
+    False
+    >>> div_by_primes_under_no_lambda(10)(12)
+    True
+    >>> div_by_primes_under_no_lambda(5)(1)
+    False
+    """
+    def checker(x):
+        return False
+    i = ____________________________
+    while ____________________________:
+        if not checker(i):
+            def outer(____________________________):
+                def inner(____________________________):
+                    return ____________________________
+                return ____________________________
+            checker = ____________________________
+        i = ____________________________
+    return ____________________________
+
+
+def insert_into_all(item, nested_list):
+    """Return a new list consisting of all the lists in nested_list,
+    but with item added to the front of each. You can assume that
+     nested_list is a list of lists.
+
+    >>> nl = [[], [1, 2], [3]]
+    >>> insert_into_all(0, nl)
+    [[0], [0, 1, 2], [0, 3]]
+    """
+    "*** YOUR CODE HERE ***"
+
+
+def subseqs(s):
+    """Return a nested list (a list of lists) of all subsequences of S.
+    The subsequences can appear in any order. You can assume S is a list.
+
+    >>> seqs = subseqs([1, 2, 3])
+    >>> sorted(seqs)
+    [[], [1], [1, 2], [1, 2, 3], [1, 3], [2], [2, 3], [3]]
+    >>> subseqs([])
+    [[]]
+    """
+    if ________________:
+        ________________
+    else:
+        ________________
+        ________________
+
+
+def non_decrease_subseqs(s):
+    """Assuming that S is a list, return a nested list of all subsequences
+    of S (a list of lists) for which the elements of the subsequence
+    are strictly nondecreasing. The subsequences can appear in any order.
+
+    >>> seqs = non_decrease_subseqs([1, 3, 2])
+    >>> sorted(seqs)
+    [[], [1], [1, 2], [1, 3], [2], [3]]
+    >>> non_decrease_subseqs([])
+    [[]]
+    >>> seqs2 = non_decrease_subseqs([1, 1, 2])
+    >>> sorted(seqs2)
+    [[], [1], [1], [1, 1], [1, 1, 2], [1, 2], [1, 2], [2]]
+    """
+    def subseq_helper(s, prev):
+        if not s:
+            return ____________________
+        elif s[0] < prev:
+            return ____________________
+        else:
+            a = ______________________
+            b = ______________________
+            return insert_into_all(________, ______________) + ________________
+    return subseq_helper(____, ____)
+
+
+def num_trees(n):
+    """Returns the number of unique full binary trees with exactly n leaves. E.g.,
+
+    1   2        3       3    ...
+    *   *        *       *
+       / \      / \     / \
+      *   *    *   *   *   *
+              / \         / \
+             *   *       *   *
+
+    >>> num_trees(1)
+    1
+    >>> num_trees(2)
+    1
+    >>> num_trees(3)
+    2
+    >>> num_trees(8)
+    429
+
+    """
+    "*** YOUR CODE HERE ***"
+
+
+def remove_odd_indices(lst, odd):
+    """Remove elements of lst that have odd indices. Use recursion!
+
+    >>> s = [1, 2, 3, 4]
+    >>> t = remove_odd_indices(s, True)
+    >>> s
+    [1, 2, 3, 4]
+    >>> t
+    [1, 3]
+    >>> l = [5, 6, 7, 8]
+    >>> m = remove_odd_indices(l, False)
+    >>> m
+    [6, 8]
+    >>> remove_odd_indices([9, 8, 7, 6, 5, 4, 3], False)
+    [8, 6, 4]
+    >>> remove_odd_indices([2], False)
+    []
+    >>> # Do not use while/for loops!
+    >>> from construct_check import check
+    >>> # ban iteration
+    >>> check(HW_SOURCE_FILE, 'remove_odd_indices',
+    ...       ['While', 'For'])
+    True
+    """
+    "*** YOUR CODE HERE ***"
+
+
+def subset_sum(target, lst):
+    """Returns True if it is possible to add some of the integers in lst
+    to get target.
+
+    >>> subset_sum(10, [-1, 5, 4, 6])
+    True
+    >>> subset_sum(4, [5, -2, 12])
+    False
+    >>> subset_sum(-3, [5, -2, 2, -2, 1])
+    True
+    >>> subset_sum(0, [-1, -3, 15])     # Sum up none of the numbers to get 0
+    True
+    """
+    "*** YOUR CODE HERE ***"
+
+
+def card(n):
+    """Return the playing card numeral as a string for a positive n <= 13."""
+    assert type(n) == int and n > 0 and n <= 13, "Bad card n"
+    specials = {1: 'A', 11: 'J', 12: 'Q', 13: 'K'}
+    return specials.get(n, str(n))
+
+
+def shuffle(cards):
+    """Return a shuffled list that interleaves the two halves of cards.
+
+    >>> shuffle(range(6))
+    [0, 3, 1, 4, 2, 5]
+    >>> suits = ['H', 'D', 'S', 'C']
+    >>> cards = [card(n) + suit for n in range(1,14) for suit in suits]
+    >>> cards[:12]
+    ['AH', 'AD', 'AS', 'AC', '2H', '2D', '2S', '2C', '3H', '3D', '3S', '3C']
+    >>> cards[26:30]
+    ['7S', '7C', '8H', '8D']
+    >>> shuffle(cards)[:12]
+    ['AH', '7S', 'AD', '7C', 'AS', '8H', 'AC', '8D', '2H', '8S', '2D', '8C']
+    >>> shuffle(shuffle(cards))[:12]
+    ['AH', '4D', '7S', '10C', 'AD', '4S', '7C', 'JH', 'AS', '4C', '8H', 'JD']
+    >>> cards[:12]  # Should not be changed
+    ['AH', 'AD', 'AS', 'AC', '2H', '2D', '2S', '2C', '3H', '3D', '3S', '3C']
+    """
+    assert len(cards) % 2 == 0, 'len(cards) must be even'
+    half = _______________
+    shuffled = []
+    for i in _____________:
+        _________________
+        _________________
+    return shuffled
+
+
+def trade(first, second):
+    """Exchange the smallest prefixes of first and second that have equal sum.
+
+    >>> a = [1, 1, 3, 2, 1, 1, 4]
+    >>> b = [4, 3, 2, 7]
+    >>> trade(a, b) # Trades 1+1+3+2=7 for 4+3=7
+    'Deal!'
+    >>> a
+    [4, 3, 1, 1, 4]
+    >>> b
+    [1, 1, 3, 2, 2, 7]
+    >>> c = [3, 3, 2, 4, 1]
+    >>> trade(b, c)
+    'No deal!'
+    >>> b
+    [1, 1, 3, 2, 2, 7]
+    >>> c
+    [3, 3, 2, 4, 1]
+    >>> trade(a, c)
+    'Deal!'
+    >>> a
+    [3, 3, 2, 1, 4]
+    >>> b
+    [1, 1, 3, 2, 2, 7]
+    >>> c
+    [4, 3, 1, 4, 1]
+    >>> d = [1, 1]
+    >>> e = [2]
+    >>> trade(d, e)
+    'Deal!'
+    >>> d
+    [2]
+    >>> e
+    [1, 1]
+    """
+    m, n = 1, 1
+
+    equal_prefix = lambda: ______________________
+    while _______________________________:
+        if __________________:
+            m += 1
+        else:
+            n += 1
+
+    if equal_prefix():
+        first[:m], second[:n] = second[:n], first[:m]
+        return 'Deal!'
+    else:
+        return 'No deal!'
+
+
+def str_interval(x):
+    """Return a string representation of interval x."""
+    return '{0} to {1}'.format(lower_bound(x), upper_bound(x))
+
+
+def add_interval(x, y):
+    """Return an interval that contains the sum of any value in interval x and
+    any value in interval y."""
+    lower = lower_bound(x) + lower_bound(y)
+    upper = upper_bound(x) + upper_bound(y)
+    return interval(lower, upper)
+
+
+def interval(a, b):
+    """Construct an interval from a to b."""
+    assert a <= b, 'Lower bound cannot be greater than upper bound'
+    return [a, b]
+
+
+def lower_bound(x):
+    """Return the lower bound of interval x."""
+    "*** YOUR CODE HERE ***"
+
+
+def upper_bound(x):
+    """Return the upper bound of interval x."""
+    "*** YOUR CODE HERE ***"
+
+
+def str_interval(x):
+    """Return a string representation of interval x."""
+    return '{0} to {1}'.format(lower_bound(x), upper_bound(x))
+
+
+def add_interval(x, y):
+    """Return an interval that contains the sum of any value in interval x and
+    any value in interval y."""
+    lower = lower_bound(x) + lower_bound(y)
+    upper = upper_bound(x) + upper_bound(y)
+    return interval(lower, upper)
+
+
+def mul_interval(x, y):
+    """Return the interval that contains the product of any value in x and any
+    value in y."""
+    p1 = x[0] * y[0]
+    p2 = x[0] * y[1]
+    p3 = x[1] * y[0]
+    p4 = x[1] * y[1]
+    return [min(p1, p2, p3, p4), max(p1, p2, p3, p4)]
+
+
+def sub_interval(x, y):
+    """Return the interval that contains the difference between any value in x
+    and any value in y."""
+    "*** YOUR CODE HERE ***"
+
+
+def div_interval(x, y):
+    """Return the interval that contains the quotient of any value in x divided by
+    any value in y. Division is implemented as the multiplication of x by the
+    reciprocal of y."""
+    "*** YOUR CODE HERE ***"
+    reciprocal_y = interval(1 / upper_bound(y), 1 / lower_bound(y))
+    return mul_interval(x, reciprocal_y)
+
+
+def par1(r1, r2):
+    return div_interval(mul_interval(r1, r2), add_interval(r1, r2))
+
+
+def par2(r1, r2):
+    one = interval(1, 1)
+    rep_r1 = div_interval(one, r1)
+    rep_r2 = div_interval(one, r2)
+    return div_interval(one, add_interval(rep_r1, rep_r2))
+
+
+def check_par():
+    """Return two intervals that give different results for parallel resistors.
+
+    >>> r1, r2 = check_par()
+    >>> x = par1(r1, r2)
+    >>> y = par2(r1, r2)
+    >>> lower_bound(x) != lower_bound(y) or upper_bound(x) != upper_bound(y)
+    True
+    """
+    r1 = interval(1, 1)  # Replace this line!
+    r2 = interval(1, 1)  # Replace this line!
+    return r1, r2
+
+
+def preorder(t):
+    """Return a list of the entries in this tree in the order that they
+    would be visited by a preorder traversal (see problem description).
+
+    >>> numbers = tree(1, [tree(2), tree(3, [tree(4), tree(5)]), tree(6, [tree(7)])])
+    >>> preorder(numbers)
+    [1, 2, 3, 4, 5, 6, 7]
+    >>> preorder(tree(2, [tree(4, [tree(6)])]))
+    [2, 4, 6]
+    """
+    "*** YOUR CODE HERE ***"
+
+
+def repeated(t, k):
+    """Return the first value in iterator T that appears K times in a row.
+    Iterate through the items such that if the same iterator is passed into
+    the function twice, it continues in the second call at the point it left
+    off in the first.
+
+    >>> s = iter([10, 9, 10, 9, 9, 10, 8, 8, 8, 7])
+    >>> repeated(s, 2)
+    9
+    >>> s2 = iter([10, 9, 10, 9, 9, 10, 8, 8, 8, 7])
+    >>> repeated(s2, 3)
+    8
+    >>> s = iter([3, 2, 2, 2, 1, 2, 1, 4, 4, 5, 5, 5])
+    >>> repeated(s, 3)
+    2
+    >>> repeated(s, 3)
+    5
+    >>> s2 = iter([4, 1, 6, 6, 7, 7, 8, 8, 2, 2, 2, 5])
+    >>> repeated(s2, 3)
+    2
+    """
+    assert k > 1
+    "*** YOUR CODE HERE ***"
+
+
+def hailstone(n):
+    """Yields the elements of the hailstone sequence starting at n.
+
+    >>> for num in hailstone(10):
+    ...     print(num)
+    10
+    5
+    16
+    8
+    4
+    2
+    1
+    """
+    "*** YOUR CODE HERE ***"
+
+
+def change_abstraction(change):
+    """
+    For testing purposes.
+    >>> change_abstraction(True)
+    >>> change_abstraction.changed
+    True
+    """
+    change_abstraction.changed = change
+
+
+change_abstraction.changed = False
+
+
+# Tree ADT
+
+def tree(label, branches=[]):
+    """Construct a tree with the given label value and a list of branches."""
+    if change_abstraction.changed:
+        for branch in branches:
+            assert is_tree(branch), 'branches must be trees'
+        return {'label': label, 'branches': list(branches)}
+    else:
+        for branch in branches:
+            assert is_tree(branch), 'branches must be trees'
+        return [label] + list(branches)
+
+
+def label(tree):
+    """Return the label value of a tree."""
+    if change_abstraction.changed:
+        return tree['label']
+    else:
+        return tree[0]
+
+
+def branches(tree):
+    """Return the list of branches of the given tree."""
+    if change_abstraction.changed:
+        return tree['branches']
+    else:
+        return tree[1:]
+
+
+def is_tree(tree):
+    """Returns True if the given tree is a tree, and False otherwise."""
+    if change_abstraction.changed:
+        if type(tree) != dict or len(tree) != 2:
+            return False
+        for branch in branches(tree):
+            if not is_tree(branch):
+                return False
+        return True
+    else:
+        if type(tree) != list or len(tree) < 1:
+            return False
+        for branch in branches(tree):
+            if not is_tree(branch):
+                return False
+        return True
+
+
+def is_leaf(tree):
+    """Returns True if the given tree's list of branches is empty, and False
+    otherwise.
+    """
+    return not branches(tree)
+
+
+def print_tree(t, indent=0):
+    """Print a representation of this tree in which each node is
+    indented by two spaces times its depth from the root.
+
+    >>> print_tree(tree(1))
+    1
+    >>> print_tree(tree(1, [tree(2)]))
+    1
+      2
+    >>> numbers = tree(1, [tree(2), tree(3, [tree(4), tree(5)]), tree(6, [tree(7)])])
+    >>> print_tree(numbers)
+    1
+      2
+      3
+        4
+        5
+      6
+        7
+    """
+    print('  ' * indent + str(label(t)))
+    for b in branches(t):
+        print_tree(b, indent + 1)
+
+
+def copy_tree(t):
+    """Returns a copy of t. Only for testing purposes.
+
+    >>> t = tree(5)
+    >>> copy = copy_tree(t)
+    >>> t = tree(6)
+    >>> print_tree(copy)
+    5
+    """
+    return tree(label(t), [copy_tree(b) for b in branches(t)])
